1e4740c16016fae8942335fd68b853d0a61e0c80
Mohit Mendiratta <mohitmendi1990@gmail.com>
2019-05-13 11:52:57 +0530

    changes done

---
+++ 1.js
@@ -0,0 +1,22 @@
+//console.log("hello");
+var x,y,z,name;
+const place ="gurgaon";
+x = 10;
+y=2;
+z=5;
+
+//x = prompt("Enter the value of x");
+//y= promt("Enter the value of y");
+//name =x+y;
+console.log((x+y)/z);
+
+
+var test="10";
+
+if(test===10)
+{
+    console.log("true");
+}
+else{
+    console.log("false");
+}
