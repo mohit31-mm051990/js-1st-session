aa7f349a11bd544b27c77b9f19beb36129f6a144
Mohit Mendiratta <mohitmendi1990@gmail.com>
2019-05-16 09:35:58 +0530

    file 2 added

---
+++ 2.js
@@ -0,0 +1,17 @@
+//var name = prompt("Enter your name");
+
+//alert("hello Mr." + name);
+
+function promptMessage(){
+   var name=prompt("Enter your name");
+    return name;
+}
+
+function alertMessage(){
+   alert("Hello" + " " + name);
+    
+
+}
+
+var name = promptMessage();
+var alert =alertMessage();
